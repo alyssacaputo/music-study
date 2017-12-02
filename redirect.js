a38809a17ec0e85903f1a209b4d3d4dbb1c4caa0
alyssacaputo <alyssa.caputo16@gmail.com>
2017-12-02 14:16:01 -0500

    Created redirect.js
    Redirects participant to the correct Google form

---
+++ redirect.js
@@ -0,0 +1,28 @@
+var randomNumber = Math.floor(Math.random * 8);
+switch(randomNumber){
+  case 0: 
+    window.location.replace(https://tinyurl.com/acstudyX1);
+    break;
+  case 1: 
+    window.location.replace(https://tinyurl.com/acstudyX2);
+    break;
+  case 2: 
+    window.location.replace(https://tinyurl.com/acstudyX3);
+    break;
+  case 3: 
+    window.location.replace(https://tinyurl.com/acstudyX4);
+    break;
+  case 4: 
+    window.location.replace(https://tinyurl.com/acstudyY1);
+    break;
+  case 5: 
+    window.location.replace(https://tinyurl.com/acstudyY2);
+    break;
+  case 6: 
+    window.location.replace(https://tinyurl.com/acstudyY3);
+    break;
+  case 7: 
+    window.location.replace(https://tinyurl.com/acstudyY4);
+    break;
+}
+  
