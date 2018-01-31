bf0b503055ae397c6cadf905fd675566a3d390d2
alyssacaputo <alyssa.caputo16@gmail.com>
2018-01-31 15:04:00 -0500

    Added all google form links

--- redirect.js
+++ redirect.js
@@ -5,28 +5,28 @@ window.alert("before switch");
 switch(randomNumber){
   case 0: 
     window.alert("reached 0");
-    //window.location.replace('https://tinyurl.com/ac-study0');
+    window.location.replace('https://drive.google.com/open?id=1UKct_NGAMdIiawU7Nm4buGwL4WuyEBYR7Wy3kAfvSGo');
     break;
   case 1: 
-    window.location.replace(https://tinyurl.com/acstudyX2);
+    window.location.replace('https://drive.google.com/open?id=1MyrCj8SRe7jR7pUTTjaY0lLmvbT0p3vz0Cor_bESUds');
     break;
   case 2: 
-    window.location.replace(https://tinyurl.com/acstudyX3);
+    window.location.replace('https://drive.google.com/open?id=1dyrUBw9EU-e_z4_0kAiiC_ulfE555pSfmBijOIUs_Sg');
     break;
   case 3: 
-    window.location.replace(https://tinyurl.com/acstudyX4);
+    window.location.replace('https://drive.google.com/open?id=1iWNEmxTxbg35Dnpo1kXd6s2At8cXvJhec67xdot5EnI');
     break;
   case 4: 
-    window.location.replace(https://tinyurl.com/acstudyY1);
+    window.location.replace('https://drive.google.com/open?id=15yhYEN2OEvcr7HbYeDCTM7FM94Mx1S10iY5NJUsztBU');
     break;
   case 5: 
-    window.location.replace(https://tinyurl.com/acstudyY2);
+    window.location.replace('https://drive.google.com/open?id=1J7DQBQTbKCEwXjjK2fZDi6CiOE-tpdbOEND5SHPueWA');
     break;
   case 6: 
-    window.location.replace(https://tinyurl.com/acstudyY3);
+    window.location.replace('https://drive.google.com/open?id=15T4hmWg1Qq3j5PmgoVj8I6B3gbVArXleIFlJyrkFYCc');
     break;
   case 7: 
-    window.location.replace(https://tinyurl.com/acstudyY4);
+    window.location.replace('https://drive.google.com/open?id=1fVo0RRe2tYrsXt1ZuDh4NWf3oeOE05rvJtfiZoPklcM');
     break;
 }
   
